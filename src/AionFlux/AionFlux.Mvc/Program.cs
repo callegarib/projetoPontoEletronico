ddbf0b51264c75fab9d44d490579b8c8afe2e05c
Beatriz Callegari <callegari.bia@gmail.com>
2024-02-05 20:47:13 -0300

    Login

--- src/AionFlux/AionFlux.Mvc/Program.cs
+++ src/AionFlux/AionFlux.Mvc/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using PontoEletronico;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<AdminCartaoPonto>(); 
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(options =>
+{
+    options.LoginPath = "/Login/Index"; // Set the login page URL
+    options.LogoutPath = "/Login/Logout"; // Set the logout page URL
+});
 
 var app = builder.Build();
 
